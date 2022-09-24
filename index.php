abf1e8a402d142ac5d7ac58297689e733bca1ba9
N30r0n <97868096+n30r0n@users.noreply.github.com>
2022-09-24 19:24:38 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookie-Master</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+    <input type="hidden" name="name">
+    <input type="hidden" name="cookie">
+    </form>
+</body>
+</html>
 <?php
-echo '<h1>Hello World</h1>';
+echo $_POST['name'];
+echo $_POST['cookie'];
 ?>
