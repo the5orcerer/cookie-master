cf89673f4f144c572105f56deb2aafe361a24dce
Fermin <97868096+the5orcerer@users.noreply.github.com>
2022-12-17 20:41:01 +0600

    Delete requfi.js

--- requfi.js
+++
@@ -1,9 +0,0 @@
-var url = "https://48ab183fac47e3.lhr.life";
-
-var xhr = new XMLHttpRequest();
-xhr.open("GET", url, true);
-
-//Send the proper header information along with the request
-xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-xhr.send();
