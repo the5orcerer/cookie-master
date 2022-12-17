d244f0534f868c30cf613d11e13a965ffaa0ab81
Fermin <97868096+the5orcerer@users.noreply.github.com>
2022-12-17 20:40:48 +0600

    Delete cookies.py

--- cookies.py
+++
@@ -1,8 +0,0 @@
-import requests
-dd = {
-    'name': 'admin',
-    'cookie': 'password=123'
-}
-req = requests.post('https://cookie-monstar.herokuapp.com/index.php', data=dd)
-print(req.text)
-print(req.status_code)
